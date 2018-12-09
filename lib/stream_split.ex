ad8af92a37e2b2f80a28fae5871037f00d2c080e
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-12-09 15:07:45 +0100

    first version

--- lib/stream_split.ex
+++ lib/stream_split.ex
@@ -1,18 +1,47 @@
 defmodule StreamSplit do
-  @moduledoc """
-  Documentation for StreamSplit.
-  """
+  defstruct(
+    device: nil,
+    buffer: [],
+    split_token: ","
+  )
 
-  @doc """
-  Hello world.
+  def split(file, split_token) do
+    Stream.resource(
+      fn ->
+        {:ok, fd} = File.open(file, [:binary, :read])
+        %StreamSplit{device: fd, buffer: [], split_token: split_token}
+      end,
+      fn (%StreamSplit{} = state) ->
+        read_next(state)
+      end,
+      fn %StreamSplit{device: device} ->
+        File.close(device)
+      end
+    )
+  end
 
-  ## Examples
+  ##### PRIVATE #####
+  defp read_next(%StreamSplit{device: fd, buffer: buffer} = state) do
+    case IO.read(fd, 4096) do
+      :eof ->
+        halt_stream(state)
+      {:error, _reason} ->
+        halt_stream(state)
+      data ->
+        try_split(state, buffer <> data)
+    end
+  end
 
-      iex> StreamSplit.hello()
-      :world
+  defp try_split(%StreamSplit{split_token: token} = state, data) do
+    case String.split(data, token, parts: 2, trim: true) do
+      [a, b] ->
+        {[a], %{state | buffer: b}}
+      _ ->
+        read_next(%{state | buffer: data})
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp halt_stream(state) do
+    {:halt, state}
   end
 end
