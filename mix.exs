8081cca96ca151446a94b0edf4567d9638f19622
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-12-09 19:12:41 +0100

    dialyzer

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule StreamSplit.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.14", only: :dev}
     ]
