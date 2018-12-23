7252c6520b7d39afb0d1a38c4ae1054ee077d88d
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-12-23 23:30:05 +0100

    try fix test

--- test/performace_test.exs
+++ test/performace_test.exs
@@ -5,31 +5,32 @@ defmodule StreamSplitPerfTest do
     File.mkdir("tmp_perf")
 
     on_exit(fn ->
-      File.rm_rf("tmp_perf/")
+      #File.rm_rf("tmp_perf/")
     end)
   end
 
   test "stream 1" do
-    {:ok, fd} = File.open("/home/meox/test.xml.gz", [:read, :binary])
+    {:ok, fd} = File.open("/home/meox/test.xml.gz", [:read, :binary, :compressed])
 
     fd
-    |> StreamSplit.split("</doc>", tagging: true)
+    |> StreamSplit.split("</doc>", tagging: true, drop_last: true)
     |> Stream.map(fn
       {:first, data} ->
         data =
           data
-          |> String.replace_prefix("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "")
-          |> String.upcase(data)
+          |> String.replace_prefix("<?xml version=\"1.0\" encoding=\"UTF-8\"?><add><doc>", "")
+          |> String.upcase()
 
-        "#{data}</doc>"
+        "<doc>#{data}</doc>"
 
       {:last, data} ->
         data =
           data
+          |> String.replace_prefix("<doc>", "")
           |> String.replace_suffix("</add>", "")
           |> String.upcase()
 
-        "#{data}</doc>"
+        "<doc>#{data}</doc>"
 
       data ->
         "#{data}</doc>"
